cfde19df7b89731c78a51721472626f1173949bc
Andrés Mayo <aemc.64@gmail.com>
2026-02-01 08:33:49 +0100

    Prevent loader

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,15 +5,18 @@ public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private int levelBuildIndex = -1;
     [SerializeField] private int spawnPointIndex = -1;
+
+    private bool _loaded = false;
     
     private void OnTriggerEnter(Collider other)
     {
+        if (_loaded) return;
         if (!other.CompareTag("Player")) return;
         if (levelBuildIndex > 0)
         {
             GeneralManager.Instance.SceneController.LoadLevel(levelBuildIndex);
+            GameSession.Instance.SpawnPointIndex = spawnPointIndex;
+            _loaded = true;
         }
-        
-        GameSession.Instance.SpawnPointIndex = spawnPointIndex;
     }
 }
